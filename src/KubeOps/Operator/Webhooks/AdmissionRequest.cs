528e89956e6e6268085862e1cdb6770ed4e19052
Mark Lopez <m@silvenga.com>
2022-05-24 09:23:33 -0500

    Switched to local sub-module for dotnet-kubernetes-client.

--- src/KubeOps/Operator/Webhooks/AdmissionRequest.cs
+++ src/KubeOps/Operator/Webhooks/AdmissionRequest.cs
@@ -1,21 +1,14 @@
-﻿using System.Text.Json.Serialization;
-
-namespace KubeOps.Operator.Webhooks;
+﻿namespace KubeOps.Operator.Webhooks;
 
 internal sealed class AdmissionRequest<TEntity>
 {
-    [JsonPropertyName("uid")]
     public string Uid { get; init; } = string.Empty;
 
-    [JsonPropertyName("operation")]
     public string Operation { get; init; } = string.Empty;
 
-    [JsonPropertyName("object")]
     public TEntity? Object { get; set; }
 
-    [JsonPropertyName("oldObject")]
     public TEntity? OldObject { get; set; }
 
-    [JsonPropertyName("dryRun")]
     public bool DryRun { get; set; }
 }
